f7999f63a83e744a462d35d09d2720907c5e97a8
onnymm <phr2807@gmail.com>
2026-02-09 10:55:10 -0700

    Implementado paquete dotenvironment

---
+++ src/ccc_utils/_settings/_env.py
@@ -0,0 +1,4 @@
+from dotenvironment import DotEnvironment
+from .._constants import PROJECT_PREFIX
+
+env = DotEnvironment(PROJECT_PREFIX)
--- src/ccc_utils/_settings/_variable_presets.py
+++
@@ -1,5 +0,0 @@
-from ccc_utils._typing import _VariableConfig
-
-class _PRESETS:
-    class SPREADSHEET:
-        GOOGLE_CLOUD_JSON_CREDENTIALS: _VariableConfig[str] = ('SPREADSHEETS_SERVICE_JSON_CREDENTIALS', 'spreadsheets_service_credentials', str)
